113e797fa4135bcbfeaef4c13b47b38b19cc0528
fliege01 <fliege01@users.noreply.github.com>
2015-08-17 10:33:20 +0200

    Update listeners.js

--- listeners.js
+++ listeners.js
@@ -6,10 +6,29 @@ module.exports = function() {
 
 var listeners = {
   init : function(){
+    global.modules = {};
     var moduledir = './modules',
         staticdir = './static';
         
     var modulesfs = global.core.fs.readdirSync(moduledir);
-    console.log ('Es wurden folgende Module erkannt: %o ', modulesfs);
+    console.log ('Es wurden folgende Module erkannt: ', modulesfs);
+    for (modulename in modulesfs){
+      var jsexists = global.core.fs.existsSync(moduledir + '/' + modulename + '/index.js');
+      var jsonexists = global.core.fs.existsSync(moduledir + '/' + modulename + '/package.json');
+        if(jsexists && jsonexists){
+          var jsonobj = JSON.parse(global.core.fs.readFileSync(moduledir + '/' + modulename + '/package.json'));
+          global.modules[modulename] = require(moduledir + '/' + modulename + '/index.js');
+          if(typeof global.modules[modulename] === 'fucntion'){
+            global.core.app.use(jsonobj.usepath, global.modules[modulename]);
+          }else{
+            console.log('Das Modul "%s" konnte nicht korrekt geladen werden. Es wird eine Funktion benötigt');
+          }
+        }else{
+          console.log('Es fehlt für das Modul "%s" die index.js/package.json');
+        }
+     
+      
+      
+    }
   }
 }
