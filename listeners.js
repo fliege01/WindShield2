9ac85cc3faaa1ce66f36f9a88370b3f687a8bf63
fliege01 <fliege01@users.noreply.github.com>
2015-08-17 09:44:30 +0200

    Update listeners.js

--- listeners.js
+++ listeners.js
@@ -1,3 +1,15 @@
-module.exports = function() {
+"use strict";
 
+module.exports = function() {
+  listeners.init();
+}
+
+var listeners = {
+  init : function(){
+    var moduledir = './modules',
+        staticdir = './static';
+        
+    var modulesfs = global.core.fs.readdirSync(moduledir);
+    console ('Es wurden folgende Module erkannt: %o ', modulefs);
+  }
 }
