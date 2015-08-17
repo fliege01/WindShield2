1ec3924714c9cfe4d77b4745373bc7a81e19df0e
fliege01 <fliege01@users.noreply.github.com>
2015-08-17 09:51:32 +0200

    Update listeners.js

--- listeners.js
+++ listeners.js
@@ -10,6 +10,6 @@ var listeners = {
         staticdir = './static';
         
     var modulesfs = global.core.fs.readdirSync(moduledir);
-    console ('Es wurden folgende Module erkannt: %o ', modulesfs);
+    console.log ('Es wurden folgende Module erkannt: %o ', modulesfs);
   }
 }
