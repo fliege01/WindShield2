3de630b94bd40ebd378a13e54ce3048725682106
fliege01 <fliege01@users.noreply.github.com>
2015-08-17 15:41:19 +0200

    Update listeners.js

--- listeners.js
+++ listeners.js
@@ -33,7 +33,7 @@ var listeners = {
       
     });
     global.core.app.get('/', function (req, res) {
-      res.redirect('/static/index.html');
+      res.redirect('/static/');
     });
     global.core.app.use('/static', global.core.modules.express.static(staticdir)); 
     
