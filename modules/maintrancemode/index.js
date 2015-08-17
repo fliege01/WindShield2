771319bcd0e39bb4faf9e97f5116f7eb73f65f2b
fliege01 <fliege01@users.noreply.github.com>
2015-08-17 11:28:34 +0200

    Update index.js

--- modules/maintrancemode/index.js
+++ modules/maintrancemode/index.js
@@ -1,7 +1,8 @@
 module.exports = function(req, res, next){
-let m = true;
-if(m === true){
-res.send("Wartungsmodus aktiv");
-}else{
-next();
+  var m = true;
+  if(m === true){
+    res.send("Wartungsmodus aktiv");
+  }else{
+    next();
+  }
 }
