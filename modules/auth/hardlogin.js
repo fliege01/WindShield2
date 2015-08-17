6b45e7d2d2b795d79c6181d5ea0fc811ab9074ce
fliege01 <fliege01@users.noreply.github.com>
2015-08-17 15:15:17 +0200

    Update hardlogin.js

--- modules/auth/hardlogin.js
+++ modules/auth/hardlogin.js
@@ -1,6 +1,7 @@
 "use strict";
 
 module.exports = function(req, res, next){
+  var out = {};
   var newuuid = global.core.modules.uuid.v4();
     var checkid = global.core.modules.uuid.v4();
     res.cookie('WSSESSION', newuuid , { maxAge: 900000, signed: true });
