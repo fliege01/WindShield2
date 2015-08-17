feb705effa142e29ae29191991709bb854bd2156
fliege01 <fliege01@users.noreply.github.com>
2015-08-17 09:19:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
+"use strict";
+/* Multicore Support */
+var cluster = require ('cluster');
+if (cluster.isMaster){ 
+  var numCPUs = require('os').cpus().length;
+  console.log("Es wurden %i CPU-Kerne gefunden", numCPUs);
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+}else{
+  /* Main Process */
 global.core = {
   init : function(){
-    console.log(this);
+    this.config = require('./config.js');
+    this.modules.express = require('express');
+    this.modules.mysql = require('mysql');
+    this.modules.cookieParser = require('cookie-parser');
+    this.modules.bodyParser = require('body-parser');
+    this.modules.crypto = require('crypto');
+    this.date = new Date();
+    this.pool = this.modules.mysql.createPool({ // Create MySQL-Connection-Pool
+      connectionLimit : 100,
+      host            : this.config.mysqlhost,
+      user            : this.config.mysqluser,
+      password        : this.config.mysqlpass,
+      database        : this.config.mysqlname
+    });
+    this.app = this.modules.express();
+    this.app.use(this.modules.cookieParser()); // Cookie-Pharser included
+    this.app.use(this.modules.bodyParser.json()); // Enable JSON-body support
+    this.app.use(this.modules.bodyParser.urlencoded({     // to support URL-encoded bodies
+      extended: true
+    })); 
+  
   },
-  port : 7085
+  modules : {}
+  
 
 }
 
 global.core.init();
+
+}
