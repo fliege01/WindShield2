3567319c0d4e21eb605c7361af805126b43c57bb
fliege01 <fliege01@users.noreply.github.com>
2015-08-17 15:12:41 +0200

    Create hardlogin.js

---
+++ modules/auth/hardlogin.js
@@ -0,0 +1,17 @@
+"use strict";
+
+module.exports = function(req, res, next){
+  var newuuid = global.core.modules.uuid.v4();
+    var checkid = global.core.modules.uuid.v4();
+    res.cookie('WSSESSION', newuuid , { maxAge: 900000, signed: true });
+    res.cookie('WSCHECK', checkid , { maxAge: 900000, signed: true });
+    
+    global.core.db.insert(
+      "INSERT INTO `WS2`.`session` (`sessionid`, `userid`, `checkid`) VALUES (?, ? , ? );", 
+      [newuuid,1,checkid]
+    );
+    out = {
+      message : 'Logged In'
+    }
+    res.send(JSON.stringify(out));
+}
