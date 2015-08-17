33a781ed1714b43a5d5a26895780ba433208f717
fliege01 <fliege01@users.noreply.github.com>
2015-08-17 10:37:19 +0200

    Update listeners.js

--- listeners.js
+++ listeners.js
@@ -13,7 +13,7 @@ var listeners = {
     var modulesfs = global.core.fs.readdirSync(moduledir);
     console.log ('Es wurden folgende Module erkannt: ', modulesfs);
     console.log(modulesfs);
-    modulesfs.forEach({
+    modulesfs.forEach(function(modulename){
       var jsexists = global.core.fs.existsSync(moduledir + '/' + modulename + '/index.js');
       var jsonexists = global.core.fs.existsSync(moduledir + '/' + modulename + '/package.json');
         if(jsexists && jsonexists){
