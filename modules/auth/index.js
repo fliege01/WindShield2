00685acdf586e906fd61216914cbefbfb32652ae
fliege01 <fliege01@users.noreply.github.com>
2015-08-17 13:55:05 +0200

    Update index.js

--- modules/auth/index.js
+++ modules/auth/index.js
@@ -5,12 +5,26 @@ var out = {};
 
 switch (requrl[0]){
   case 'info':
+    
+    
+    
+    
     out = {
         status : 4001,
         message : "You're not logged in"
       };
+    break;
     
+  case 'hardlogin':
+    var newuuid = global.core.modules.uuid.v4();
+    var checkid = global.core.modules.uuid.v4();
+    res.cookie('WSSESSION', newuuid , { maxAge: 900000, signed: true });
+    res.cookie('WSCHECK', newuuid , { maxAge: 900000, signed: true });
     
+    global.core.db.insert(
+      "INSERT INTO `WS2`.`session` (`sessionid`, `userid`, `checkid`) VALUES (?, ? , ? );", 
+      [newuuid,1,checkid]
+    );
     break;
 }
 
