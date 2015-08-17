d829200a95e347a524d89db4ca850f5c8e805d0d
fliege01 <fliege01@users.noreply.github.com>
2015-08-17 12:32:49 +0200

    Update database.js

--- database.js
+++ database.js
@@ -1,4 +1,24 @@
 var database = {
-
+  select : function(query, vals, callback){
+    var validsql = global.core.modules.mysql.format(query, vals);
+    global.core.pool.query(validsql, function(err, rows, fields) {
+      if (err) throw err;
+      var info = {
+        sql : validsql,
+      }
+      callback(rows, fields, info);
+    });
+  }
+  
+  insert : function(query, vals, callback){
+    var validsql = global.core.modules.mysql.format(query, vals);
+    global.core.pool.query(validsql, {title: 'test'}, function(err, result) {
+      if (err) throw err;
+      var info = {
+        sql : validsql
+      }
+      callback(result, info);
+    });
+  }
 }
 module.exports = database;
