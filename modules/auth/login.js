f5f7f8c23e1aaf4bac051b7b51cb353ccdb67e12
fliege01 <fliege01@users.noreply.github.com>
2015-08-18 08:47:07 +0200

    Update login.js

--- modules/auth/login.js
+++ modules/auth/login.js
@@ -4,6 +4,27 @@ module.exports = function(req, res, next){
   var out = {};
   if(req.method == 'POST' && typeof req.body.username !== 'undefined' && typeof req.body.password !== 'undefined'){
     if(global.core.moduleExists('passwordcrypt')){
+      global.core.db.select(
+        "SELECT * FROM `users` WHERE `username` LIKE ? ",
+        [req.body.username],
+        function(rows, fields, info){
+          if(global.modules['passwordcrypt'].verify(rows[0].password, req.body.password)){
+            out = {
+              status : 200,
+              message : "login success"
+            };
+            res.send(JSON.stringify(out));
+          }else{
+            out = {
+              code : 403,
+              message : 'login canceled'
+            };
+            res.send(JSON.stringify(out));
+          }
+          
+        }
+        )
+      
       
     }else{
     out = {
