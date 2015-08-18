86813e851932834d7989b455994ea041d506ee52
fliege01 <fliege01@users.noreply.github.com>
2015-08-18 14:13:22 +0200

    Update login.js

--- modules/auth/login.js
+++ modules/auth/login.js
@@ -8,7 +8,7 @@ module.exports = function(req, res, next){
         "SELECT * FROM `user` WHERE `username` LIKE ? ",
         [req.body.username],
         function(rows, fields, info){
-          console.log(rows);
+          if(typeof rows[0] !== 'undefined'){
           if(global.modules['passwordcrypt'].verify(rows[0].password, req.body.password)){
             
             var newuuid = global.core.modules.uuid.v4();
@@ -29,6 +29,13 @@ module.exports = function(req, res, next){
               message : "login success"
             };
             res.send(JSON.stringify(out));
+          }else{
+            out = {
+              code : 403,
+              message : 'login canceled'
+            };
+            res.send(JSON.stringify(out));
+          }
           }else{
             out = {
               code : 403,
