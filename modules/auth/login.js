46ab5a4d17dbaf1b0217894268e2014b6159337e
fliege01 <fliege01@users.noreply.github.com>
2015-08-18 07:45:03 +0200

    Update login.js

--- modules/auth/login.js
+++ modules/auth/login.js
@@ -1,1 +1,14 @@
 "use strict";
+
+module.exports = function(req, res, next){
+  var out = {};
+  if(req.method == 'POST' && typeof req.body.username !== 'undefined' && typeof req.body.password !== 'undefined'){
+    // Run
+  }else{
+    out = {
+      code : 403,
+      message : 'login canceled'
+    };
+    res.send(JSON.stringify(out));
+  }
+}
