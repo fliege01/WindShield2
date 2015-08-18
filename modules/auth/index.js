8afcd06c01f4c9b88c7c8531c50f7771089ae1bb
fliege01 <fliege01@users.noreply.github.com>
2015-08-18 09:53:52 +0200

    Update index.js

--- modules/auth/index.js
+++ modules/auth/index.js
@@ -3,6 +3,7 @@
 var CMDinfo = require('./info.js');
 var CMDhardlogin = require('./hardlogin.js');
 var CMDlogin = require('./login.js');
+var CMDlogout = require('./logout.js');
 
 module.exports = function(req, res, next){
 var requrl = req.url.split('/').filter(Boolean);
@@ -18,6 +19,10 @@ switch (requrl[0]){
   
   case 'login':
     CMDlogin(req, res, next);
+    break;
+    
+  case 'logout':
+    CMDlogout(req, res, next);
     break;
     
   default :
