c72786aa9435a8f7739ab004f326bde6fae9fdbb
fliege01 <fliege01@users.noreply.github.com>
2015-08-18 09:53:03 +0200

    Update logout.js

--- modules/auth/logout.js
+++ modules/auth/logout.js
@@ -1,5 +1,18 @@
 "use strict";
 
 module.exports = function(req, res, next){
-
+global.core.db.delete(
+  "DELETE FROM `session` WHERE `session`.`sessionid` = ? AND `session`.`checkid` = ?",
+  [req.signedCookies.WSSESSION, req.signedCookies.WSCHECK],
+  function(result, info){
+    res.clearCookie('WSSESSION');
+    res.clearCookie('WSCHECK');
+    var out = {
+      status : 200,
+      message : "logout success"
+    };
+    res.send(JSON.stringify(out));
+  }
+  
+  )
 };
