1846bc2100f9298dea8c81af0d4cfbbef67eeb9b
fliege01 <fliege01@users.noreply.github.com>
2015-08-18 14:41:57 +0200

    Update include.js

--- modules/auth/include.js
+++ modules/auth/include.js
@@ -1,7 +1,22 @@
 "use strict";
 
-module.exports = function(req){
+module.exports = function(){
   var include = {
-    
+    hasPermission : function(req, name, callback){
+      var permissions = {};
+      global.core.db.select(
+        "SELECT `permissions`.`permission` as `name`, `permissions`.`value` FROM `permissions`, `session` WHERE `session`.`userid` = `permissions`.`userid` AND `session`.`sessionid` = ? AND `session`.`checkid` = ? ;",
+        [req.signedCookies.WSSESSION, req.signedCookies.WSCHECK],
+        function(rows, fields, info){
+          for(row in rows){
+            permissions[row.name] = row.value;
+          }
+          if(permissions[name] == 1){
+            callback(true);
+          }else{
+            callback(false);
+          }
+        });
+    }
   };
 };
