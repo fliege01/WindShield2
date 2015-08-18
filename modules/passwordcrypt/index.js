ad4130f3dcd56d83bd3f5c61024b3f91ee3211ad
fliege01 <fliege01@users.noreply.github.com>
2015-08-18 08:14:51 +0200

    Update index.js

--- modules/passwordcrypt/index.js
+++ modules/passwordcrypt/index.js
@@ -1,5 +1,24 @@
 "use strict";
 
 module.exports = function(){
-
+  return {
+    verify : function(hash, pass){
+      var passarr = hash.split("."),
+          salt = passarr[0],
+          hash = passarr[1],
+          passhash = global.core.modules.crypto.createHash('sha1').update(salt + pass).digest('hex');
+      if(passhash === hash){
+        return true;
+      }else{
+        return false;
+      }
+      
+    },
+    generate : function(pass){
+      var salt = global.core.modules.crypto.randomBytes(Math.ceil(25/2)).toString('hex').slice(0,25),
+        password = salt +  pass,
+        hash = global.core.modules.crypto.createHash('sha1').update(password).digest('hex');
+      return salt + '.' + hash;
+    }
+  };
 }
