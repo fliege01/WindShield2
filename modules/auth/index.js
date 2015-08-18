7348eddca6801de2cfef7a5707e621cd67f54fb1
fliege01 <fliege01@users.noreply.github.com>
2015-08-18 10:06:07 +0200

    Update index.js

--- modules/auth/index.js
+++ modules/auth/index.js
@@ -7,9 +7,6 @@ var CMDlogout = require('./logout.js');
 
 module.exports = function(req, res, next){
 var requrl = req.url.split('/').filter(Boolean);
-if(typeof requrl[0] !== 'undefined'){
-  requrl[0] = '';
-}
 switch (requrl[0]){
   case 'info':
     CMDinfo(req, res, next);
