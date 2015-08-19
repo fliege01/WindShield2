05a5a9ed5be88ed467b3e43535b5c4c9d3c351b9
fliege01 <fliege01@users.noreply.github.com>
2015-08-19 08:27:50 +0200

    Update showall.js

--- modules/api/user/showall.js
+++ modules/api/user/showall.js
@@ -3,10 +3,10 @@
 module.exports = function(req, res, next){
 
       global.core.db.select(
-        "SELECT `user`.`id`,`user`.`username`,`user`.`name` FROM `user`;",
+        "SELECT `user`.`id`, `user`.`username` FROM `user`;",
         [],
         function(err, row, fields) {
           if (err) throw err;
           res.send(JSON.stringify(row));
         });
-}
+};
