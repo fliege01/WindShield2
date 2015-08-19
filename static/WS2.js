9d146bef6c6be754315b17e9c25ee5c8ea545077
fliege01 <fliege01@users.noreply.github.com>
2015-08-19 12:55:39 +0200

    Update WS2.js

--- static/WS2.js
+++ static/WS2.js
@@ -18,8 +18,16 @@ var WS = {
   },
   objectMerge : function(obj1, obj2){
     var obj3 = {};
-    for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
-    for (var attrname in obj2) { obj3[attrname] = obj2[attrname]; }
+    for (var attrname in obj1) {
+      obj3[attrname] = obj1[attrname]; 
+      console.log(attrname);
+      console.log(obj1.attrname);
+    }
+    for (var attrname in obj2) {
+      obj3[attrname] = obj2[attrname]; 
+      console.log(attrname);
+      console.log(obj2.attrname);
+    }
     return obj3;
   }
   
