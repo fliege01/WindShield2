7cfbd08c8090e9a21955b2296a4cbd83fa1076a2
fliege01 <fliege01@users.noreply.github.com>
2015-08-19 07:30:31 +0200

    Update index.js

--- modules/api/index.js
+++ modules/api/index.js
@@ -1,9 +1,15 @@
 "use strict";
+var CMDuserindex = require('./user');
+
+
 
 module.exports = function(req, res, next){
 var requrl = req.url.split('/').filter(Boolean);
 switch (requrl[0]){
-  
+  case 'user':
+    
+    
+    break;
   default :
     res.send('');
 }
