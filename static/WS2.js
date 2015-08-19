bcabc66e83323a69cc3b493e020534be90fbf807
fliege01 <fliege01@users.noreply.github.com>
2015-08-19 12:35:45 +0200

    Update WS2.js

--- static/WS2.js
+++ static/WS2.js
@@ -1,1 +1,29 @@
-
+var WS = {
+  modal = function(innerHtml, params, callback){
+    if(typeof innerhtml !== 'undefined'){ this.innerHtml = innerHtml; }else{ this.innerHtml = '[DUMMY]'; }
+    this.params = {
+      title : '[DUMMY]',
+      buttontext : '[DUMMY]',
+      editable : true,
+      cancelable : true,
+      onButtonClick : null,
+      onCancelClick : null,
+      onEditClick : null,
+      onShowFinish : null
+    };
+    if(typeof params !== 'undefined' && typeof params === 'object'){
+      WS.objectMerge(this.params, params);
+    } 
+      
+    
+  },
+  objectMerge : function(obj1, obj2){
+    var obj3 = {};
+    for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
+    for (var attrname in obj2) { obj3[attrname] = obj2[attrname]; }
+    return obj3;
+  }
+  
+  
+  
+};
