db1bb4c9a158c8ee67e959c2447a7181e3697f02
fliege01 <fliege01@users.noreply.github.com>
2015-08-19 07:46:14 +0200

    Update index.js

--- modules/api/index.js
+++ modules/api/index.js
@@ -7,7 +7,7 @@ module.exports = function(req, res, next){
 var requrl = req.url.split('/').filter(Boolean);
 switch (requrl[0]){
   case 'user':
-    
+    CMDuserindex(req, res, next);
     
     break;
   default :
