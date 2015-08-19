4e9b7635963032113357217928b1ea0bc9b8bf30
fliege01 <fliege01@users.noreply.github.com>
2015-08-19 09:04:49 +0200

    Update add.js

--- modules/api/user/add.js
+++ modules/api/user/add.js
@@ -1,5 +1,25 @@
 "user strict";
 
 module.exports = function(req, res, next){
-
+    if(req.method == 'POST'){
+      global.core.db.insert(
+        "INSERT INTO `user` (`id`, `username`, `password`) VALUES (NULL, ? , ? );",
+        [req.body.username, global.include['passwordcrypt'].generate(req.body.password)],
+        function(err) {
+            if (err) throw err;
+            var out = {
+              status : 200,
+              message : 'User added'
+            }
+            res.send(JSON.stringify(out));
+          });
+    }else{
+      var out = {
+        status : 4004,
+        message : "endpoint doesn't esists"
+      };
+      res.send(JSON.stringify(out));
+      
+    }
+  }
 }
