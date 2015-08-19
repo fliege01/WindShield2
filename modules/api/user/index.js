5d3c76160cfc308d36768acc3cf604b9e113173b
fliege01 <fliege01@users.noreply.github.com>
2015-08-19 09:17:06 +0200

    Update index.js

--- modules/api/user/index.js
+++ modules/api/user/index.js
@@ -1,10 +1,14 @@
 "use strict";
 var CMDusershowall = require('./showall.js');
+var CMDuseradd = require('./add.js');
 module.exports = function(req, res, next){
 var requrl = req.url.split('/').filter(Boolean);
 switch (requrl[1]){
+  case 'add':
+    CMDuseradd(req, res, next);
+    break;
   
   default :
-    CMDusershowall(CMDusershowall(req, res, next));
+    CMDusershowall(req, res, next);
 }
 };
