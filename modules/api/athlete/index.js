bdd194921e48f34bcbcb9e11f96915589d49c40a
fliege01 <fliege01@users.noreply.github.com>
2015-08-19 10:25:48 +0200

    Update index.js

--- modules/api/athlete/index.js
+++ modules/api/athlete/index.js
@@ -1,12 +1,12 @@
 "use strict";
 
-
+CMDathleteshowall = require('./showall.js');
 module.exports = function(req, res, next){
   var requrl = req.url.split('/').filter(Boolean);
   switch (requrl[1]){
   
    
   default :
-    
+    CMDathleteshowall(req, res, next);
   }
 }
