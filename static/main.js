06d4139a8bf3f0b0696c3d824b18b9201e8376f9
fliege01 <fliege01@users.noreply.github.com>
2015-08-20 07:55:03 +0200

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,10 +1,40 @@
 
 $( document ).ready(function() {
-WS.ajax('/auth/info', {}, function(data){
+WS.ajax('/auth/info', function(data){
   $('.loadingscreen').fadeOut(800);
   data = JSON.parse(data);
   console.log(data);
+  if(data.status == 4001){
+    // Login required
+  }else{
+    // Init software
+  }
 });  
   
 
 });
+
+function initlogin (){
+  var html = '<form><input type="text" data-form="input" class="modalinput" placeholder="Benutzername"><input data-form="input" data-enter="submit" type="password" class="modalinput" placeholder="Passwort"></form>';
+  var loginmodal = new WS.modal(html, {
+    title : 'Login',
+    buttontext : 'Login',
+    cancelable : false,
+    onButtonClick : function(e){
+      var dataset = [];
+      console.log(e.data.DOM);
+      /*var modalFormDOM = $(e.data.DOM);
+      modalFormDOM.each(function(){
+        dataset.push($(this).context.value);
+      });
+      
+      
+      WS.ajax('/auth/login', {username : username, password : password}, function(d){
+        
+      });
+      */
+    }
+  })
+}
+
+
