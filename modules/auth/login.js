513f3d750ab846827cf2dc2b9a37dbc5fd8ef55a
fliege01 <fliege01@users.noreply.github.com>
2015-08-20 08:51:00 +0200

    Update login.js

--- modules/auth/login.js
+++ modules/auth/login.js
@@ -41,7 +41,7 @@ module.exports = function(req, res, next){
           }
           }else{
             out = {
-              code : 403,
+              status : 403,
               message : 'login canceled'
             };
             res.send(JSON.stringify(out));
@@ -53,14 +53,14 @@ module.exports = function(req, res, next){
       
     }else{
     out = {
-      code : 500,
+      status : 500,
       message : 'Module missed'
     };
     res.send(JSON.stringify(out));
     }
   }else{
     out = {
-      code : 403,
+      status : 403,
       message : 'login canceled'
     };
     res.send(JSON.stringify(out));
