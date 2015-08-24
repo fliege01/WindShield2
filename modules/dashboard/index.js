4473796d984a5b5181a4a89ad1d83eb49abe035e
fliege01 <fliege01@users.noreply.github.com>
2015-08-24 15:20:19 +0200

    Update index.js

--- modules/dashboard/index.js
+++ modules/dashboard/index.js
@@ -1,5 +1,11 @@
 "use strict";
 
 module.exports = function(req, res, next){
-
+  if(req.xhr){
+    var html = "<h2>Dashboard</h2>";
+    
+    res.send(html);
+  }else{
+    // NORMAL REQUEST
+  }
 };
